caa80a2cc8ab76f5ffa6f6e097752196b84ad17c
ori-eph <oriephrati@hilma.tech>
2023-10-25 16:56:04 +0300

    update

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
-document.getElementById("counter-btn").addEventListener("click", pop);
-const counterTag = document.getElementById("counter");
-let counter = 0;
-if (!localStorage.getItem("users")) {
-    localStorage.setItem("users", []);
-}
-function pop() {
-    counter++;
-    counterTag.innerText = counter;
-    img = document.getElementById("cat-img");
-    img.src = "./media/pop-open.jpeg";
-    setTimeout(setPictureBack, 100);
-    function setPictureBack() {
-        img.src = "./media/pop-closed.jpeg";
+
+const popGameApp = {
+    start() {
+        if (!localStorage.getItem("users")) {
+            localStorage.setItem("users", JSON.stringify([]));
+            const users = JSON.parse(localStorage.getItem("users"));
+            users.push({ email: "test@pop-cat.com", password: "meow123" });
+            localStorage.setItem("users", JSON.stringify(users));
+            console.log(popGameApp.validationLogin);
+            const submit = document.getElementById("submitbtn").addEventListener('click', this.validationLogin)
+        }
+    },
+
+    validationLogin() {
+        console.log("in!");
+        const password = document.getElementById('pwrd').value;
+        const email = document.getElementById('email').value;
+        users = localStorage.getItem("users");
+        for (let i = 0; i < users.length; i++) {
+            if (email === users[i].email && password === users[i].password) {
+                logInUser();
+            }
+            else {
+                document.getElementById("validateLogin").innerText = "email or pass not correct";
+            }
+        }
+    },
+
+    gameStart() {
+        // document.getElementById("counter-btn").addEventListener("click", popGameOperator);
+        const counterTag = document.getElementById("counter");
+        let counter = 0;
+    },
+
+    popGameOperator() {
+        counter++;
+        counterTag.innerText = counter;
+        img = document.getElementById("cat-img");
+        img.src = "./media/pop-open.jpeg";
+        setTimeout(setPictureBack, 100);
+        function setPictureBack() {
+            img.src = "./media/pop-closed.jpeg";
+        }
     }
 }
+
+window.onload = popGameApp.start;
